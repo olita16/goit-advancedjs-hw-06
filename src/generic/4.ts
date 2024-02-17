929008e346e0628839093411451e149cce5ea74c
olita16 <119816415+olita16@users.noreply.github.com>
2024-02-17 23:28:03 +0100

    first commit tasks baseTypes and generik

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,16 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface PageInterface {
+  title: string;
 }
 
-class Page extends Component {
-  pageInfo () {
+class Component<T> {
+  constructor(public props: T) {}
+}
+
+class Page extends Component<PageInterface> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
